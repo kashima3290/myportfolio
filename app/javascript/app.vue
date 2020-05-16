5ae6c47346290d2583a8d09e056fce382d1b2172
kashimaraiki <raiki3290@gmail.com>
2020-05-16 21:39:16 +0900

    portfolio 開示ボタン設置

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,64 @@
 <template>
   <v-app id="app">
-    <Header/>
+    <v-container
+      class="fill-height"
+      fluid
+      style="min-height: 434px"
+    >
+      <v-fade-transition mode="out-in">
+        <v-row v-if="show" key="0">
+          <v-col cols="6">
+            <v-card>
+              <v-img
+                src="https://cdn.pixabay.com/photo/2016/10/17/14/31/background-1747781_1280.jpg"
+                height="125"
+                class="grey darken-4"
+              ></v-img>
+              <v-card-title class="title">height</v-card-title>
+            </v-card>
+          </v-col>
+
+          <v-col cols="6">
+            <v-card>
+              <v-img
+                src="https://cdn.pixabay.com/photo/2016/10/17/14/31/background-1747781_1280.jpg"
+                height="125"
+                contain
+                class="grey darken-4"
+              ></v-img>
+              <v-card-title class="title">height with contain</v-card-title>
+            </v-card>
+          </v-col>
+
+          <v-col cols="6">
+            <v-card>
+              <v-img
+                src="https://cdn.pixabay.com/photo/2016/10/17/14/31/background-1747781_1280.jpg"
+                max-height="125"
+                class="grey darken-4"
+              ></v-img>
+              <v-card-title class="title">max-height</v-card-title>
+            </v-card>
+          </v-col>
+
+          <v-col cols="6">
+            <v-card>
+              <v-img
+                src="/assets/portfolio1.jpg"
+                max-height="125"
+                contain
+                class="grey darken-4"
+              ></v-img>
+              <v-card-title class="title">max-height with contain</v-card-title>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <v-row v-else key="1" justify="center">
+          <v-btn text @click="show = true">Load images</v-btn>
+        </v-row>
+      </v-fade-transition>
+    </v-container>
   </v-app>
 </template>
 
@@ -10,6 +68,7 @@ import Header from "./components/Header";
 export default {
   data: function () {
     return {
+      show: false,
     }
   },
   components: {
--- app/javascript/components/Header.vue
+++ app/javascript/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <header id="header">
-    <v-app-bar>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-toolbar-title>This is Header.vue</v-toolbar-title>
-    </v-app-bar>
+    <!-- <v-app-bar> -->
+      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+      <!-- <v-toolbar-title>This is Header.vue</v-toolbar-title> -->
+    <!-- </v-app-bar> -->
   </header>
 </template>
 
